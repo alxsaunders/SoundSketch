a047cbea186fd7514c69c01fddcc4f27639d2bce
Alex <104272847+alxsaunders@users.noreply.github.com>
2025-01-20 07:33:32 -0500

    added navbar and icons/ logo placeholder

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserCircle, Info } from 'lucide-react';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white">
-      <div className="max-w-7xl mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <Link to="/" className="font-bold">Logo</Link>
-          <div className="flex space-x-4">
-            <Link to="/" className="hover:text-gray-300">Home</Link>
-            <Link to="/shop" className="hover:text-gray-300">Shop</Link>
-            <Link to="/about" className="hover:text-gray-300">About</Link>
-            <Link to="/contact" className="hover:text-gray-300">Contact</Link>
-          </div>
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Logo on left */}
+        <Link to="/" className="navbar-logo">
+          LOGO
+        </Link>
+
+        {/* Icons on right */}
+        <div className="navbar-icons">
+          <Link to="/profile" className="navbar-icon">
+            <UserCircle size={32} />
+          </Link>
+          <Link to="/info" className="navbar-icon">
+            <Info size={32} />
+          </Link>
         </div>
       </div>
     </nav>
